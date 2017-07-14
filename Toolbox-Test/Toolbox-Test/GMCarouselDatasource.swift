cff976850cfe5f3527eea8afea322f78b130ab72
Gastón Montes <gastonmontes@hotmail.com>
2017-07-14 16:56:37 -0300

    Gaston - Read data file.

--- Toolbox-Test/Toolbox-Test/GMCarouselDatasource.swift
+++ Toolbox-Test/Toolbox-Test/GMCarouselDatasource.swift
@@ -7,7 +7,29 @@
 //
 
 import UIKit
+import SwiftyJSON
 
 class GMCarouselDatasource: NSObject {
-
+    // MARK: - Constants.
+    private let kCarouselDataFileName = "Toolbox-Test-Data"
+    private let kCarouselDataFileExtension = "json"
+    
+    // MARK: - Initialization.
+    override init() {
+        super.init()
+        
+        self.readCarouselDataFile()
+    }
+    
+    private func readCarouselDataFile() {
+        if let dataFilePath = Bundle.main.path(forResource:kCarouselDataFileName, ofType:kCarouselDataFileExtension) {
+            do {
+                let fileData = try Data(contentsOf: URL(fileURLWithPath: dataFilePath), options: .alwaysMapped)
+                let jsonData = JSON(data: fileData)
+                print(jsonData)
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        }
+    }
 }
