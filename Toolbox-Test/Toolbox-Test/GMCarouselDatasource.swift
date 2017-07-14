e4cf9f4e12874af3c48b4d493ca3a4e103dcf25d
Gastón Montes <gastonmontes@hotmail.com>
2017-07-14 14:10:36 -0300

    Gaston - Add carousel datasource file.

---
+++ Toolbox-Test/Toolbox-Test/GMCarouselDatasource.swift
@@ -0,0 +1,13 @@
+//
+//  GMCarouselDatasource.swift
+//  Toolbox-Test
+//
+//  Created by Gaston Montes on 7/14/17.
+//  Copyright © 2017 Gaston Montes. All rights reserved.
+//
+
+import UIKit
+
+class GMCarouselDatasource: NSObject {
+
+}
