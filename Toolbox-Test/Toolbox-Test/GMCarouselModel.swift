fec72d68f387c7bfc8dfb414d499f992318ce319
Gastón Montes <gastonmontes@hotmail.com>
2017-07-17 12:20:32 -0300

    Gaston - Add carousel model item file.

--- Toolbox-Test/Toolbox-Test/GMCarouselModel.swift
+++ Toolbox-Test/Toolbox-Test/GMCarouselModel.swift
@@ -18,17 +18,19 @@ public enum GMCarouselModelType {
 private let kCarouselModelTitleKey = "title"
 private let kCarouselModelTypeKey = "type"
 private let kCarouselModelTypePosterString = "poster"
+private let kCarouselModelItemKey = "items"
 
 class GMCarouselModel {
     // MARK: - Vars.
     private(set) var carouselModelTitle: String!
     private var carouselModelType: GMCarouselModelType!
-    private(set) var carouselModelItems = Array<Any>()
+    private(set) var carouselModelItems = Array<GMCarouselModelItem>()
     
     // MARK: - Initialization.
     init(carouselJSONData: JSON) {
         self.carouselModelTitle = carouselJSONData[kCarouselModelTitleKey].stringValue
         self.carouselModelType = self.carouselModelType(stringType: carouselJSONData[kCarouselModelTypeKey].stringValue)
+        self.addCarouselModelItems(items: carouselJSONData[kCarouselModelItemKey].arrayValue)
     }
     
     // MARK: - Data functions.
@@ -38,5 +40,11 @@ class GMCarouselModel {
         }
         
         return GMCarouselModelType.typeThumb
+    }
+    
+    private func addCarouselModelItems(items: Array<JSON>) -> Void {
+        for itemData in items {
+            self.carouselModelItems.append(GMCarouselModelItem(modelItemJSONData: itemData))
+        }
     }
 }
---
+++ Toolbox-Test/Toolbox-Test/GMCarouselModelItem.swift
@@ -0,0 +1,17 @@
+//
+//  GMCarouselModelItem.swift
+//  Toolbox-Test
+//
+//  Created by Gaston Montes on 7/17/17.
+//  Copyright © 2017 Gaston Montes. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+class GMCarouselModelItem {
+    // MARK: - Initialization.
+    init(modelItemJSONData: JSON!) {
+        
+    }
+}
