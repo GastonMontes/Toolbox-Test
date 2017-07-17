2163b6bd9b9f0cbc2ad291a9824275bbf280a914
Gastón Montes <gastonmontes@hotmail.com>
2017-07-17 12:38:12 -0300

    Gaston - Get carousel model item data from JSON.

--- Toolbox-Test/Toolbox-Test/GMCarouselModelItem.swift
+++ Toolbox-Test/Toolbox-Test/GMCarouselModelItem.swift
@@ -9,9 +9,21 @@
 import Foundation
 import SwiftyJSON
 
+// MARK: - Constants.
+private let kModelItemTitleKey = "title"
+private let kModelItemImageKey = "url"
+private let kModelItemVideoKey = "video"
+
 class GMCarouselModelItem {
+    // MARK: - Vars.
+    private(set) var modelItemTitle: String!
+    private(set) var modelItemImageURL: String!
+    private(set) var modelItemVideoURL: String?
+    
     // MARK: - Initialization.
     init(modelItemJSONData: JSON!) {
-        
+        self.modelItemTitle = modelItemJSONData[kModelItemTitleKey].stringValue
+        self.modelItemImageURL = modelItemJSONData[kModelItemImageKey].stringValue
+        self.modelItemVideoURL = modelItemJSONData[kModelItemVideoKey].stringValue;
     }
 }
